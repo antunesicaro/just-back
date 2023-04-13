4e4ebdc7c5b8a29b0027f2e66757e5337abf3015
antunesicaro <icarodasilva.12@icloud.com>
2023-04-12 23:33:23 -0300

    logics socketio

--- server.js
+++ server.js
@@ -38,6 +38,41 @@ const io = require("socket.io")(server, {
   },
 });
 
+io.on('connection',(socket) => {
+  console.log(`usuario tu se conectou com o servidor socket hein, olha o id : ${socket.id}`)
+
+  socket.on('create-new-room',(data) => { //fica escutando aqui o evento create-new-room, quando escuta lá, ou seja, o cliente emit(emite) aqui vai ser escutado e manipulo os dadis (data) do cliente q é o wss.js
+    createNewRoomHandler(data,socket)
+  })
+})
+
+const createNewRoomHandler = (data,socket) => {
+  console.log('host ta criando a sala')
+  const {identity} = data;
+
+  const roomId = uuidv4();
+
+  // criar novo usuario
+  const newUser = {
+    identity,
+    id:uuidv4(),
+    socketId: socket.id,
+    roomId:roomId
+  }
+
+  //push no user para ir para usarioconectado q é o array conectedusers
+  connectedUsers = [...connectedUsers,newUser];
+
+  //cria nova sala
+  const newRoom = {
+    id:roomId,
+    connectedUsers: [newUser]
+  }
+
+  // entra na sala do socketio pra conectar só numa sala em especifico
+  //logica aqui do socket
+}
+
 server.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
